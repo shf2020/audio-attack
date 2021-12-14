37e166596251a421e515cd1d411032952cfbf93f
shf2020 <sun1998@mail.ustc.edu.cn>
2021-12-14 11:08:16 +0800

    update

--- download_checkpoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-test -d ckpts || mkdir ckpts
-cd ckpts
-wget http://download.tensorflow.org/models/speech_commands_v0.01.zip
-unzip speech_commands_v0.01.zip
-rm -f speech_commands_v0.01.zip
--- extract_times.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cat $1 | grep Attack | sed -r 's/^.*in ([0-9][0-9]*.[0-9][0-9][0-9][0-9]) seconds/\1/g'
